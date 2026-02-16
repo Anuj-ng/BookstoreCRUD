0d44bfb5b8259126eb55ee4d9b5e5c9b0a0821cd
Anuj <anujsharma135790@gmail.com>
2026-02-16 15:16:08 +0530

    connect frontend to deployed backend

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const bookBaseUrl = axios.create({
-  baseURL: "http://localhost:8000/book/",
+  baseURL: "https://bookstore-backend-zo61.onrender.com/book/",
 });
