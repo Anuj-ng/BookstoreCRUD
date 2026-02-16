06cfa05d1cd34a2f883562769015e0836806f509
Anuj <anujsharma135790@gmail.com>
2026-02-16 15:14:00 +0530

    enable cors for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,13 @@ const cors = require("cors");
 databaseConnection();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use("/book", bookRouter);
-const PORT=process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Port Listening On ${PORT}`);
 });
